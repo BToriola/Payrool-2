ffa467bf300016ec696774b374fae1626b847561
Rele <adenrelebabatunde@gmail.com>
2020-07-12 19:50:17 +0100

    Updates

---
+++ precache-manifest.8fed25aa2d91f9af04721604fe5d0260.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4e3259e8ee20dbab245a8182c2a458",
+    "url": "/Payrool-2/index.html"
+  },
+  {
+    "revision": "3a0c35239afa7bfedcd5",
+    "url": "/Payrool-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac39d5904f2e614fbbcb",
+    "url": "/Payrool-2/static/js/2.45e99d75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Payrool-2/static/js/2.45e99d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0c35239afa7bfedcd5",
+    "url": "/Payrool-2/static/js/main.4c140ad6.chunk.js"
+  },
+  {
+    "revision": "0d69333da40bbf2e51ac",
+    "url": "/Payrool-2/static/js/runtime-main.8200abcf.js"
+  },
+  {
+    "revision": "5a0bbf91c0d512824aadfd4e3a0f56d4",
+    "url": "/Payrool-2/static/media/Abiola.5a0bbf91.jpeg"
+  },
+  {
+    "revision": "9ab44bb4f85727b187c458c61cb3f0fe",
+    "url": "/Payrool-2/static/media/KimberylyLogo.9ab44bb4.png"
+  },
+  {
+    "revision": "b384079c97809d459e4870b8571fbf3b",
+    "url": "/Payrool-2/static/media/biddy.b384079c.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Payrool-2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "403da293f8a8c5cc0773316a179f796d",
+    "url": "/Payrool-2/static/media/rele.403da293.jpeg"
+  }
+]);
